7ce6e25f06304d7a225800156b1425392aa24bb3
avi <avi10137@gmail.com>
2026-06-24 10:28:11 +0300

    start project

---
+++ cfiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+namespace program;
+
+class files_system
+{
+
+    static void Main()
+    {
+        string path = @"C:\Users\User\csfiles\manage_file\reports.txt";
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("file not exist");
+        }
+    }
+}
