7a8d5da73ab781853c86ed55ac9917bc224433c0
avi <avi10137@gmail.com>
2026-06-24 11:13:59 +0300

    read from file complete

--- cfiles.cs
+++ cfiles.cs
@@ -4,13 +4,52 @@ namespace program;
 
 class files_system
 {
-
+    static List<string> loadFile (string path)
+    {
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("Eror: File reports.txt not found");
+            return [];
+        }
+        
+        string [] allLines = File.ReadAllLines(path);
+        List<string> resultAllLines = new List<string>();
+        foreach(string line in allLines)
+        {
+            resultAllLines.Add(line);
+        }
+        if (resultAllLines.Count == 0)
+        {
+            Console.WriteLine("Eror: File is empty");
+        }
+        return resultAllLines;
+    }
     static void Main()
     {
         string path = @"..\..\..\reports.txt";
-        if (!File.Exists(path))
+        List<string> allFiles = loadFile(path);
+        int counter_files = allFiles.Count;
+        if (counter_files > 0)
         {
-            Console.WriteLine("file not exist");
+            Console.WriteLine($"file loaded :{counter_files} lines found ");
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
+        
+
+     
+
+
     }
 }
