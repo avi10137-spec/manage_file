06451436998b1201f0ecffe9787654dc73d9739d
avi <avi10137@gmail.com>
2026-06-24 10:43:06 +0300

    create reports txt

--- cfiles.cs
+++ cfiles.cs
@@ -7,7 +7,7 @@ class files_system
 
     static void Main()
     {
-        string path = @"C:\Users\User\csfiles\manage_file\reports.txt";
+        string path = @"..\..\..\reports.txt";
         if (!File.Exists(path))
         {
             Console.WriteLine("file not exist");
